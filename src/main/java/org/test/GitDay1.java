2a326cf9e2c4c8aaa3190773c73b38d947f23c86
WeslinJino <weslinjino55@gmail.com>
2024-11-23 21:58:31 +0530

    New project Automation commit

---
+++ src/main/java/org/test/GitDay1.java
@@ -0,0 +1,20 @@
+package org.test;
+
+import java.lang.reflect.Method;
+
+public class GitDay1 {
+	 private void method1() {
+		System.out.println("1");
+
+	}
+	 private void Method2() {
+		System.out.println("2");
+
+	}
+	 private void Method3() {
+		System.out.println("3");
+
+	}
+	
+
+}
